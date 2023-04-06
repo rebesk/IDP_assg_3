4bd04d7491132e482f703997452241a9a5591044
Rebecca Eskekärr <rebecca@eskekarr.se>
2023-04-06 10:58:00 +0200

    Skapat en primär planering

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,10 +1,97 @@
-﻿namespace Program
+﻿using System.Globalization;
+
+namespace MJU23v_DTP_T1
 {
-    internal class Program
+    public class Language
     {
-        static void Main(string[] args)
+        public string family, group;
+        public string language, area, link;
+        public int pop;
+        public Language(string line)
         {
-            Console.WriteLine("Hello, World!");
+            string[] field = line.Split("|");
+            family = field[0];
+            group = field[1];
+            language = field[2];
+            pop = (int)double.Parse(field[3], CultureInfo.InvariantCulture);
+            area = field[4];
+            link = field[5];
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Language {language}:");
+            Console.Write($"  family: {family}");
+            if (group != "")
+                Console.Write($">{group}");
+            Console.WriteLine($"\n  population: {pop}");
+            Console.WriteLine($"  area: {area}");
+        }
+    }
+    public class Program
+    {
+        static string dir = @"..\..\..";
+        static List<Language> eulangs = new List<Language>();
+        static void Main(string[] arg)
+        {
+            using (StreamReader sr = new StreamReader($"{dir}\\lang.txt"))
+            {
+                Language lang;
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    // Console.WriteLine(line);
+                    lang = new Language(line);
+                    eulangs.Add(lang);
+                    line = sr.ReadLine();
+                }
+            }
+            Console.WriteLine("==== Languages in Spain ====");
+            foreach (Language L in eulangs)
+            {
+                int index = L.area.IndexOf("Spain");
+                if (index != -1)
+                    L.Print();
+            }
+            Console.WriteLine("==== Baltic Languages ====");
+            foreach (Language L in eulangs)
+            {
+                int index = L.group.IndexOf("Baltic");
+                if (index != -1)
+                    L.Print();
+            }
+            Console.WriteLine("==== Population larger than 50 millions ====");
+            foreach (Language L in eulangs)
+            {
+                if (L.pop >= 50_000_000)
+                    L.Print();
+            }
+            Console.WriteLine("==== Number of Germanics ====");
+            int sumgerm = 0;
+            foreach (Language L in eulangs)
+            {
+                int index = L.group.IndexOf("Germanic");
+                if (index != -1)
+                    sumgerm += L.pop;
+            }
+            Console.WriteLine($"Germanic speaking population: {sumgerm}");
+            Console.WriteLine("==== Number of Romance ====");
+            int sumromance = 0;
+            foreach (Language L in eulangs)
+            {
+                int index = L.group.IndexOf("Romance");
+                if (index != -1)
+                    sumromance += L.pop;
+            }
+            Console.WriteLine($"Romance speaking population: {sumromance}");
         }
     }
 }
+/* Lista planering:
+ * 1 . Skapa en meny med 'quit'
+ * 2 . Gör en 'help' metod
+ * 3 . Skapa metod för list country countryname , en lista för vilka språk som talas i det landet
+ * 4 . Skapa en metod för show country countryname , visa information som cirkulerar det angivna landet
+ * 5 . Skapa en metod för show language (liknande punkt 4 metodmässigt) , visa information om språket
+ * 6 . Skapa en metod för list group group name , visa länder som tillhör den angivna språkgruppen
+ * 7 . Skriv in resterande fråge-kommandon med //NYI - kommmentarer
+ */
